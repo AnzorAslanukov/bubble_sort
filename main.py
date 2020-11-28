5d3f94850892f41914cba29fe46f91c660940826
AnzorAslanukov <aslanukov96@gmail.com>
2020-11-27 23:41:33 -0500

    A push to test if the user's name has changed

--- main.py
+++ main.py
@@ -14,13 +14,20 @@ upper_frame = tk.Frame(root).pack()
 lower_frame = tk.Frame(root).pack()
 
 # Label showing generated array
-gen_arr_lbl = tk.Label(upper_frame, text='', bg='red')
+gen_arr_lbl = tk.Label(upper_frame, text='Something', bg='red')
 gen_arr_lbl.config(width=75)
 gen_arr_lbl.pack()
 
 # Set text for sorted_arr_lbl
+sorted_arr_lbl = tk.Label(lower_frame, text='')
+sorted_arr_lbl.pack()
 
-
+test_list = test.list_of_lists
+expanding_var = ''
+for list in test_list:
+    expanding_var += str(list) + '\n'
+    sorted_arr_lbl['text'] = expanding_var
+    time.sleep(.5)
 
 # Wrap-up of GUI
 root.title('Bubble Sort')
