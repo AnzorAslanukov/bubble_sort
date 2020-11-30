52bac29f162d3bcf707cf40ed0dc18ab1c7931d5
AnzorAslanukov <aslanukov96@gmail.com>
2020-11-30 05:50:47 -0500

    First fully working version.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-import test
 import random
 
 '''
@@ -9,14 +8,31 @@ Random Array
 
 # Generates a random array of "arr_len" length
 def random_arr_gen():
-    arr_len = int(arr_length_entry.get())
-    random_arr = []
-    for i in range(0, arr_len):
-        random_arr.append(random.randint(0, 100))
+    len_input = arr_length_entry.get()
+    if len_input.isnumeric() and 2 <= int(len_input) <= 20:
+        error_label.pack_forget()
+        error_label_desc.pack_forget()
+        arr_len = int(arr_length_entry.get())
+        random_arr = []
 
-    generated_array_label['text'] = random_arr
+        for i in range(0, arr_len):
+            random_arr.append(random.randint(0, 100))
+        generated_array_label['text'] = random_arr
 
-    return random_arr
+        for x in range(0, len(random_arr)):
+            for j in range(0, len(random_arr) - 1):
+                if random_arr[j] > random_arr[j + 1]:
+                    temp_var = random_arr[j]
+                    random_arr[j] = random_arr[j + 1]
+                    random_arr[j + 1] = temp_var
+
+        sorted_arr_lbl['text'] = random_arr
+
+    else:
+        error_label.pack()
+        error_label_desc.pack(pady=5)
+        generated_array_label['text'] = ''
+        sorted_arr_lbl['text'] = ''
 
 
 '''
@@ -28,10 +44,11 @@ root = tk.Tk()
 
 # Contains label which shows generated array
 upper_frame = tk.Frame(root)
-upper_frame.pack()
+upper_frame.pack(padx=10)
 # Contains label which shows sorting of generated array
 middle_frame = tk.Frame(root)
-middle_frame.pack(fill=tk.X)
+middle_frame.pack(padx=10,
+                  fill=tk.X)
 # Contains GUI controls
 lower_frame = tk.Frame(root)
 lower_frame.pack()
@@ -46,14 +63,15 @@ generated_array_title_label = tk.Label(upper_frame,
                                        text='Generated Array',
                                        padx=2,
                                        pady=2,
-                                       font='Arial')
+                                       font='Arial',
+                                       width=60)
 generated_array_title_label.pack(fill=tk.X,
                                  pady=5)
 
 # Label showing generated array
 generated_array_label = tk.Label(upper_frame,
                                  anchor='center',
-                                 text='Something',
+                                 text='',
                                  padx=2,
                                  pady=2,
                                  relief='sunken',
@@ -61,36 +79,32 @@ generated_array_label = tk.Label(upper_frame,
 generated_array_label.pack(fill=tk.X,
                            pady=5)
 
-# Title label for scrollbar Listbox with sorted arrays
-scrollbar_title_label = tk.Label(middle_frame,
-                                 anchor='center',
-                                 text='Sorted Array',
-                                 font='Arial')
-scrollbar_title_label.pack(fill=tk.X,
-                           pady=5,
-                           padx=80)
+# Title label for sorted array
+sorted_arr_title = tk.Label(middle_frame,
+                            text='Sorted Array',
+                            font='Arial')
+sorted_arr_title.pack(fill=tk.X)
 
-# Scrollbar object for sorted array
-array_scrollbar = tk.Scrollbar(middle_frame)
-array_scrollbar.pack(fill=tk.Y, side='right')
-
-# Listbox object showing sorted array
-array_listbox = tk.Listbox(middle_frame,
-                           yscrollcommand=array_scrollbar.set)
-for index in range(0, len(test.list_of_lists)):
-    array_listbox.insert(index, test.list_of_lists[index])
-array_listbox.pack(fill=tk.X)
-array_scrollbar.config(command=array_listbox.yview)
+# Label for sorted array
+sorted_arr_lbl = tk.Label(middle_frame,
+                          text='',
+                          relief='sunken',
+                          font='Arial')
+sorted_arr_lbl.pack(fill=tk.X)
 
 # Int input title label for list length
 int_length_title = tk.Label(lower_frame,
                             text='Choose Array Length \n'
                                  '(Value between 2 & 50)')
-int_length_title.pack(fill=tk.X)
+int_length_title.pack(pady=5,
+                      fill=tk.X)
 
 # Error message label
 error_label = tk.Label(lower_frame,
-                       text='Error')
+                       text='ERROR')
+error_label_desc = tk.Label(lower_frame,
+                            text='Characters must be\n'
+                            'integers between 1 and 21')
 
 '''
 Text Input 
@@ -106,7 +120,7 @@ Button
 start_button = tk.Button(lower_frame,
                          text='Start',
                          command=random_arr_gen)
-start_button.pack()
+start_button.pack(pady=5)
 
 # Wrap-up of GUI
 root.title('Bubble Sort')
--- test.py
+++
@@ -1,9 +0,0 @@
-import random
-
-
-list_of_lists = []
-
-for i in range(0, 50):
-    list_of_lists.append([])
-    for j in range(0, 20):
-        list_of_lists[i].append(random.randint(0, 100))
